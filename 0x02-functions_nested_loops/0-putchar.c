f21eb86ce12dd10c44db902cd420fd7968a6c43a
Tinsel19 <olaoluwaoladunjoye130@gmail.com>
2022-12-12 23:09:50 -0800

    x

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ *  File: 0-putchar.c
+ *  Auth: Tinsel19
+ */
+
+ /**
+  * main - This code prints string _putchar
+  * Return: 0 on success
+  */
+
+int main(void)
+{
+char[] = "_putchar";
+int i;
+for (i = 0 ; i < 8 ; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
+}
