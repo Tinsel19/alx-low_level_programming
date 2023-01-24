7436eb07f42d5a59631941e7afa0f0d5d3c893b4
Tinsel19 <107304041+Tinsel19@users.noreply.github.com>
2023-01-24 07:13:14 -0800

    Tinsel

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,44 +1,49 @@
-#include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
  *
- * Description: A program that prints the opcodes
- *              of its own main function.
- *              Usage: ./main number_of_bytes
- *
- * @argc: argument counter
- * @argv: argument vector
- *
- * Return: Always Successful
-*/
-
-int main(int argc, char *argv[])
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
 {
-	int index, nbytes;
-	char *ptr = (char *) main;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+	int n;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	nbytes = atoi(argv[1]);
-	if (nbytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	for (index = 0; index < nbytes; index++)
-	{
-		printf("%02x", ptr[index] & 0xFF);
-		if (index != nbytes - 1)
-			printf(" ");
-	}
-	printf("\n");
+	print_opcodes((char *)&main, n);
 	return (0);
 }
