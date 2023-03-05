df2829b64d94369bd51fdeaf6fc6517159594037
Tinsel19 <olaoluwaoladunjoye130@gmail.com>
2023-03-05 10:06:55 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,44 +1,25 @@
+#include "main.h"
+
 /**
- * cap_string - a function that capitalizes
- *              all words of a string
- *
- * @s: pointer to char input array
- *
- * Return: @s
-*/
-
-char *cap_string(char *s)
+ * leet - Encodes a string in leet
+ * @str: String to be encoded
+ * Return: Always 0.
+ */
+char *leet(char *str)
 {
-	int i = 0;
+	int i = 0, k;
+	char s[] = "aAeEoOtTlL";
+	char s1[] = "4433007711";
 
-	/*iterate through our array values*/
-	while (s[i] != '\0')
-	{
-		/*check for any lowercase letters*/
-		if (s[i] >= 97 && s[i] <= 122)
+	for (; str[i] != '\0'; i++)
 		{
-			/**
-			 * if we have a null character
-			 * change its value to capital
-			*/
-			if (i == 0)
-			{
-				s[i] -= 32;
-			}
-			/**
-			 * if we find any character matching the below before any small
-			 * letter we change that value to a capital letter.
-			*/
-			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
-				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
-				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
-				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
-				s[i - 1] == 124)
-			{
-				s[i] -= 32;
-			}
+		for (k = 0; k <= 9; k++)
+		{
+		if (s[k] == str[i])
+		{
+		str[i] = s1[k];
 		}
-		i++;
-	}
-	return (s);
+		}
+		}
+return (str);
 }
